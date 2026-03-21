e894c8ca44713979202ae4e06e9a2cac0ab744ea
Kamil Szewczyk <kamil@logicspot.com>
2026-03-21 08:49:20 +0100

    Fixing failing Keystatic on Netlify

---
+++ src/pages/api/test.ts
@@ -0,0 +1,11 @@
+import type { APIRoute } from 'astro';
+
+export const prerender = false;
+
+export const GET: APIRoute = async () => {
+    return new Response(JSON.stringify({ ok: true }), {
+        headers: {
+            'content-type': 'application/json',
+        },
+    });
+};
